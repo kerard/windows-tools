690ef2e90305514af3ff535552a360b9d5f0ecb3
Ken <kenneth.erard@gmail.com>
2016-03-18 14:05:49 -0400

    Added Audit Configuration Script
    Sets the ACL of a filesystem object to enable auditing.

---
+++ set-auditpolicy.ps1
@@ -0,0 +1,8 @@
+﻿$aUser = "Everyone"
+$aRules = "FullControl"
+$aInheritType = "ContainerInherit,ObjectInherit"
+$aType = "Success,Failure"
+$aAccessRule = New-Object System.Security.AccessControl.FileSystemAuditRule($aUser,$aRules,$aInheritType,"None",$aType)
+$acl = Get-Acl "C:\audit-test"
+$acl.SetAuditRule($aAccessRule)
+$acl | Set-Acl "C:\audit-test"
